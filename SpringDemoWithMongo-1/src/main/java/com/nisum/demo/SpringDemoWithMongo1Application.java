37ee0888edffdd7e0e6e7506aebc5a08b59f800c
nisum <nisum@192.168.10.116>
2017-09-13 11:05:29 +0530

    empty project

--- SpringDemoWithMongo-1/src/main/java/com/nisum/demo/SpringDemoWithMongo1Application.java
+++ SpringDemoWithMongo-1/src/main/java/com/nisum/demo/SpringDemoWithMongo1Application.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.nisum.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDemoApplication {
+public class SpringDemoWithMongo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
+		SpringApplication.run(SpringDemoWithMongo1Application.class, args);
 	}
 }
